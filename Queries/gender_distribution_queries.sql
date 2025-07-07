3bb02bff2b783ff989b9b0d8fd1bb1b97581354d
lajuma <lajuma@gmx.net>
2025-07-07 17:40:16 +0200

    added queries for gender distribution for deities and mythological characters

--- Queries/gender_distribution_queries.sql
+++ Queries/gender_distribution_queries.sql
@@ -30,9 +30,76 @@ JOIN named_after_instances AS i ON i.instance_id = inst.instance_id
 JOIN named_after AS n ON i.named_after_id = n.named_after_id
 JOIN asteroids_named_after AS an ON n.named_after_id = an.named_after_id
 JOIN asteroids AS a ON a.spk_id = an.spk_id
-WHERE sub.label = 'natural person' OR sub.label = 'fictional person'
+WHERE sub.label IN ('natural person', 'fictional person')
 GROUP BY
 	sub.label,
+	n.sex_or_gender
+ORDER BY asteroid_count DESC;
+
+
+-- Q: What are the subcategories of the remaining asteroids (not included in the fictional / natural person query)
+-- A: 1) Mythological Characters: 171 male, 102 female + subcategories
+--    2) Deities: 52 male, 98 female + subcategories
+SELECT
+    sub.label,
+	n.sex_or_gender,
+    COUNT(DISTINCT a.spk_id) AS asteroid_count
+FROM asteroids AS a
+JOIN asteroids_named_after AS an ON a.spk_id = an.spk_id
+JOIN named_after AS n ON an.named_after_id = n.named_after_id
+JOIN named_after_instances AS nai ON n.named_after_id = nai.named_after_id
+JOIN instances AS inst ON nai.instance_id = inst.instance_id
+JOIN subclasses_instances AS si ON inst.instance_id = si.instance_id
+JOIN subclasses AS sub ON si.subclass_id = sub.subclass_id
+WHERE n.sex_or_gender IS NOT NULL
+  AND a.spk_id NOT IN (
+    SELECT a1.spk_id
+    FROM asteroids AS a1
+    JOIN asteroids_named_after AS an1 ON a1.spk_id = an1.spk_id
+    JOIN named_after AS n1 ON an1.named_after_id = n1.named_after_id
+    JOIN named_after_instances AS nai1 ON n1.named_after_id = nai1.named_after_id
+    JOIN instances AS inst1 ON nai1.instance_id = inst1.instance_id
+    JOIN subclasses_instances AS si1 ON inst1.instance_id = si1.instance_id
+    JOIN subclasses AS sub1 ON si1.subclass_id = sub1.subclass_id
+    WHERE sub1.label IN ('natural person', 'fictional person')
+)
+GROUP BY
+	n.sex_or_gender,
+	sub.label
+ORDER BY asteroid_count DESC;
+
+-- Q: How many male/female deities are there? (also includes subclasses like 'greek deity' or 'waterdeity')
+-- A: female: 136, male: 63
+SELECT
+	n.sex_or_gender,
+    COUNT(DISTINCT a.spk_id) AS asteroid_count
+FROM asteroids AS a
+JOIN asteroids_named_after AS an ON a.spk_id = an.spk_id
+JOIN named_after AS n ON an.named_after_id = n.named_after_id
+JOIN named_after_instances AS nai ON n.named_after_id = nai.named_after_id
+JOIN instances AS inst ON nai.instance_id = inst.instance_id
+JOIN subclasses_instances AS si ON inst.instance_id = si.instance_id
+JOIN subclasses AS sub ON si.subclass_id = sub.subclass_id
+WHERE n.sex_or_gender IS NOT NULL AND sub.label LIKE '%deity%'
+GROUP BY
+	n.sex_or_gender
+ORDER BY asteroid_count DESC;
+
+
+-- Q: How many mythological characters are there? (also includes subclasses like 'mythological greek character')
+-- A: female: 65, male: 21
+SELECT
+	n.sex_or_gender,
+    COUNT(DISTINCT a.spk_id) AS asteroid_count
+FROM asteroids AS a
+JOIN asteroids_named_after AS an ON a.spk_id = an.spk_id
+JOIN named_after AS n ON an.named_after_id = n.named_after_id
+JOIN named_after_instances AS nai ON n.named_after_id = nai.named_after_id
+JOIN instances AS inst ON nai.instance_id = inst.instance_id
+JOIN subclasses_instances AS si ON inst.instance_id = si.instance_id
+JOIN subclasses AS sub ON si.subclass_id = sub.subclass_id
+WHERE n.sex_or_gender IS NOT NULL AND sub.label LIKE '%mythological%'
+GROUP BY
 	n.sex_or_gender
 ORDER BY asteroid_count DESC;
 
